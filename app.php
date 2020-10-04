66b658cced0f9ac7a6b7fcb681433b0e973c2a28
axioma34 <32591621+axioma34@users.noreply.github.com>
2020-10-04 23:38:31 +0700

    Update app.php

--- app.php
+++ app.php
@@ -12,6 +12,7 @@ if (!$dataString) {
 
 $transactions = explode("\n", $dataString);
 $ratesData = null;
+
 try {
     $ratesData = file_get_contents('https://api.exchangeratesapi.io/latest');
 } catch (\Exception $e) {
@@ -25,34 +26,23 @@ if (!$rates || $rates && !array_key_exists('rates', $rates)) {
     throw new \Exception("Could not get rates!");
 }
 
-$rate = new \App\Entities\Rate();
-$rate->setDate(new DateTime($rates['date']));
-$rate->setBaseCurrency($rates['base']);
-$rate->setRates($rates['rates']);
-
 $commissionCompute = new \App\CommissionComputingService();
-$commissionCompute->setRate($rate);
+$commissionCompute->setRate(getRate($rates));
 
 foreach ($transactions as $transaction) {
     $transaction = json_decode($transaction, true);
     if (!validateTransaction($transaction)) continue;
-    $transactionEntity = new \App\Entities\Transaction();
-    $transactionEntity->setAmount($transaction['amount']);
-    $transactionEntity->setCurrency($transaction['currency']);
-    $transactionEntity->setBin($transaction['bin']);
-
+    $transactionEntity = getTransaction($transaction);
     try {
         $binResults = file_get_contents('https://lookup.binlist.net/' . $transactionEntity->getBin());
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
-    $binResults = json_decode($binResults,true);
-    if (!validateBin($binResults)) continue;
-    $binCountry = new \App\Entities\BinCountry();
-    $binCountry->setAlpha2($binResults['country']['alpha2']);
-    $commissionCompute->setBinCountry($binCountry);
 
-    echo $commissionCompute->compute($transactionEntity)."\n";
+    $binResults = json_decode($binResults, true);
+    if (!validateBin($binResults)) continue;
+    $commissionCompute->setBinCountry(getBinCountry($binResults));
+    echo $commissionCompute->compute($transactionEntity) . "\n";
 }
 
 function validateTransaction(?array $row): bool
@@ -63,11 +53,35 @@ function validateTransaction(?array $row): bool
     return true;
 }
 
-
 function validateBin(?array $arr)
 {
     if (!$arr || !array_key_exists('country', $arr) || !array_key_exists('alpha2', $arr['country'])) {
         return false;
     }
     return true;
+}
+
+function getTransaction(array $transaction): \App\Entities\Transaction
+{
+    $transactionEntity = new \App\Entities\Transaction();
+    $transactionEntity->setAmount($transaction['amount']);
+    $transactionEntity->setCurrency($transaction['currency']);
+    $transactionEntity->setBin($transaction['bin']);
+    return $transactionEntity;
+}
+
+function getRate(array $rateArr): \App\Entities\Rate
+{
+    $rate = new \App\Entities\Rate();
+    $rate->setDate(new DateTime($rateArr['date']));
+    $rate->setBaseCurrency($rateArr['base']);
+    $rate->setRates($rateArr['rates']);
+    return $rate;
+}
+
+function getBinCountry(array $binResults): \App\Entities\BinCountry
+{
+    $binCountry = new \App\Entities\BinCountry();
+    $binCountry->setAlpha2($binResults['country']['alpha2']);
+    return $binCountry;
 }
